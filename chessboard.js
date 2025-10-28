3696013cbe4965edfee38a483fd54964cdc81e10
Admin <you@example.com>
2025-10-28 18:53:22 +0530

    format code for readability and fix newline character in chessboard generation

--- chessboard.js
+++ chessboard.js
@@ -1,13 +1,13 @@
-let size=8
-let chessboard=''
+let size = 8
+let chessboard = ''
 for (let row = 0; row < size; row++) {
-for (let column = 0; column < size; column++) {
-    if ((row+column)%2===0) {
-        chessboard='#'
-    }else{
-        chessboard=''
+    for (let column = 0; column < size; column++) {
+        if ((row + column) % 2 === 0) {
+            chessboard += '#'
+        } else {
+            chessboard += ' '
+        }
     }
-}
-  chessboard += "/n"
+    chessboard += '\n'
 }
 console.log(chessboard)
