9201866d3783fdf71cdaa2bf3b73b628823914c6
Saman <kaursaman242@email.com>
2025-11-02 20:11:03 +0530

    feat: implement multiCall function to invoke a callback with a message multiple times

---
+++ function/25Tasks/task-19.js
@@ -0,0 +1,11 @@
+// Write a function multiCall(message, callback, times) that calls the callback with the message repeatedly using a loop.
+function multiCall(message,callback,times) {
+    for (let i = 0; i < times; i++) {
+       console.log(message)
+        callback
+    }
+}
+function printMessage(result) {
+    console.log(result)
+}
+multiCall("Hey everyone! JS is easy to learn language",printMessage,4)
