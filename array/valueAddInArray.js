46712f2bfb999f768e3ab04aace738d11707f8a5
Sahilpreetsingh25 <sahilpreet.singh@tickmark.io>
2025-11-03 12:11:56 +0530

    feat: add array traversal and value manipulation examples

---
+++ array/valueAddInArray.js
@@ -0,0 +1,28 @@
+const students=["Sahil","saman","PRaBh"] //0dfn1
+//add value in last
+students[3]="Gurpreet"
+
+// replace existing value
+students[1]="Saman"
+
+// methods to add single value value in last
+students.push("Randhir")
+// methods to add mutliples value value in last
+students.push("Manjot",3,true,["kiwi",23])
+console.log('students', students)
+
+
+const subject=["math","hindi"] //0fex2
+// subject=students
+
+// console.log('subject', subject)
+
+
+
+
+
+// console.log(newSt)
+
+
+
+
