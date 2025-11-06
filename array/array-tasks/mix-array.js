85e36bb945d2fec794cfb4c75372af5302c9b914
Saman <kaursaman242@email.com>
2025-11-06 22:32:13 +0530

    feat: implement task management and dynamic list functionality with user prompts

--- array/array-tasks/mix-array.js
+++ array/array-tasks/mix-array.js
@@ -6,7 +6,22 @@
 
 // Replace it inside the array (so the final nested array contains 80 instead of 40).
 const mix = ["car", 34, false, [10, 20, [30, 40]]];
-const value = mix[3][2][1];
-const newValue = value * 2;
-mix[3][2][1] = newValue;
+mix.forEach((item) => {
+  if (Array.isArray(item)) {
+    item.forEach((subItem) => {
+        if (Array.isArray(subItem)) {
+            subItem.forEach((innermostItem, innermostIndex, innermostArr) => {
+                if (innermostItem === 40) {
+                    innermostArr[innermostIndex] = innermostItem * 2;
+                }
+            })
+        }
+    })
+    }
+});
 console.log(mix);
+
+// const value = mix[3][2][1];
+// const newValue = value * 2;
+// mix[3][2][1] = newValue;
+// console.log(mix);
