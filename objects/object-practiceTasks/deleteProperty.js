cb493345d173513cc85ee13554e17ccb0d7019b5
Saman <kaursaman242@email.com>
2025-11-07 12:25:34 +0530

    feat: update array manipulation example and remove unused object practice files

--- objects/object-practiceTasks/deleteProperty.js
+++
@@ -1,9 +0,0 @@
-// Given:
-
-// const phone = { company: "Apple", model: "iPhone 14", price: 80000 };
-
-
-// Delete the price property.
-const phone = { company: "Apple", model: "iPhone 14", price: 80000 };
-delete phone.company
-console.log(phone)
--- objects/object-practiceTasks/updatePropertyValue.js
+++
@@ -1,9 +0,0 @@
-// Given:
-
-// const person = { name: "Aman", city: "Delhi" };
-
-
-// Change the city to "Mumbai".
-const person = { name: "Aman", city: "Delhi" };
-person.city="Mumbai"
-console.log(person)
